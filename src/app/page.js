58a76f129b854d2fceb8ff1a8be3cc54fcd58438
ferozalam <ferozalam99@example.com>
2024-01-20 20:04:13 +0600

    gitignore update

--- src/app/page.js
+++ src/app/page.js
@@ -4,9 +4,10 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main className={styles.main}>
-      
+      <div>
       <h1>Wellcome</h1>
       <p>Deltasoft</p>
+      </div>
     </main>
   );
 }
